991016c0766868348558756e056ab154e7150d8c
ktranfaglia1 <kyletranfaglia@gmail.com>
2024-09-25 17:30:08 -0400

    progressed exercise

--- Exercise01/summation.c
+++ Exercise01/summation.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
+#include <mpi.h>
 
-int main(int argc, char *argv[]) {
-    
+int main(int argc, char **argv) {
+
     // Check that a command line argument was entered
     if (argc != 2) {
         printf("Must provide a command line argument N (positive integer).\n");
@@ -17,25 +17,25 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    int my_rank, send_data, recieve_data;
+    MPI_Status status;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
     // Variables for timing and summation
     clock_t start, end;
     long sum = 0;
 
     start = clock();  // Start timing
 
-    // Sum from 1 to n
-    for (long i = 1; i <= n; i++) {
-        sum += i;
-    }
-
-    end = clock(); // End timing
+    end = clock();  // End timing
 
     // Calculate the time taken in seconds
     double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
 
+    MPI_Finalize()
+
     // Output the result
     printf("Sum from 1 to %ld = %ld\n", n, sum);
     printf("Time to compute: %f seconds\n", cpu_time_used);
-
-    return 0;
 }
