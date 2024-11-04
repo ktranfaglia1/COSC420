df403919f75139c7a10dc79c6cf0c91076b907d3
ktranfaglia1 <kyletranfaglia@gmail.com>
2024-11-04 17:26:41 -0500

    Added test programs

---
+++ TestPrograms/dynamicScheduling.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main(int argc, char *argv[]) {
+
+    #pragma omp parallel for schedule(dynamic,1)
+    for (int i = 0; i < 10; i++) {
+        int id = omp_get_thread_num();
+        printf("Thread: %d | Value: %d\n", id, i);
+    }
+}
